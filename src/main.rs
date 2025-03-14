286c74502bb83fd2c1a6f0bfb272f33dca6ca8be
Christopher Biscardi <chris@christopherbiscardi.com>
2025-03-13 23:41:35 -0700

    template with readme

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use bevy::{prelude::*, scene::SceneInstanceReady};
+use bevy_skein::SkeinPlugin;
+
+fn main() {
+    App::new()
+        .register_type::<Character>()
+        .add_plugins((
+            DefaultPlugins,
+            SkeinPlugin::default(),
+        ))
+        .add_observer(
+            // log the component from the gltf spawn
+            |trigger: Trigger<SceneInstanceReady>,
+             children: Query<&Children>,
+             characters: Query<&Character>| {
+                for entity in children
+                    .iter_descendants(trigger.entity())
+                {
+                    let Ok(character) =
+                        characters.get(entity)
+                    else {
+                        continue;
+                    };
+                    info!(?character);
+                }
+            },
+        )
+        .add_systems(Startup, startup)
+        .run();
+}
+
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+struct Character {
+    name: String,
+}
+
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(SceneRoot(asset_server.load(
+        // Change this to your exported gltf file
+        GltfAssetLabel::Scene(0).from_asset("demo.gltf"),
+    )));
+}

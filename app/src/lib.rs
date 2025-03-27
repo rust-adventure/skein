25563683121d05b9a1376b03dd12e704057be6bc
Christopher Biscardi <chris@christopherbiscardi.com>
2025-03-27 14:52:52 -0700

    set font for people who dont have it preinstalled

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -33,6 +33,9 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
             <head>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet"/>
                 <AutoReload options=options.clone()/>
                 <HydrationScripts options islands=true/>
                 <MetaTags/>

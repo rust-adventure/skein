9d2defae9bac592a0fb7bc9a5661805ffbd96729
Christopher Biscardi <chris@christopherbiscardi.com>
2025-05-04 11:52:23 -0700

    add options test for pub issues

---
+++ tests/integration.rs
@@ -0,0 +1,21 @@
+//! make sure SkeinPlugin options are all publically setable
+//! if they should be, and that the plugin constructs without issue
+#[test]
+fn test_skein_options() {
+    use bevy::prelude::*;
+    use bevy_skein::SkeinPlugin;
+
+    App::new()
+        .add_plugins((
+            MinimalPlugins,
+            SkeinPlugin { handle_brp: false },
+        ))
+        // immediately exit
+        .add_systems(
+            Startup,
+            |mut exit_event: EventWriter<AppExit>| {
+                exit_event.write(AppExit::Success);
+            },
+        )
+        .run();
+}
